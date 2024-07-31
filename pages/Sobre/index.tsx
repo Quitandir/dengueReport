a9ee308724e3449bb92a88237585a5ead5636a7e
Moisés Carniel <moises.carniel@kings.services>
2024-07-31 08:49:05 -0300

    Changed font color

--- pages/Sobre/index.tsx
+++ pages/Sobre/index.tsx
@@ -10,16 +10,16 @@ export default function Sobre() {
         <>
             <Header />
             <Box sx={{ display: 'flex', width: '90%', flexDirection: 'column', alignItems: 'center', margin: '1em auto'}}>
-                <Typography variant="h4" color='#222222' m='1em' className={rubik.className}>
+                <Typography variant="h4" color='331800' m='1em' className={rubik.className}>
                     Sobre
                 </Typography>
-                <Typography component='p' textAlign='justify' m='0.5em 0' className={rubik.className}>
+                <Typography component='p' textAlign='justify' m='0.5em 0' className={rubik.className} color='331800'>
                     Projeto desenvolvido durante aulas de Meio Ambiente e Sustentabilidade para o curso Tecnólogo em Análise e Desenvolvimento de Sistema, no campus Canoas do IFRS. 
                 </Typography>
-                <Typography component='p' textAlign='justify' m='0.5em 0' className={rubik.className}>
+                <Typography component='p' textAlign='justify' m='0.5em 0' className={rubik.className} color='331800'>
                     Após pesquisa sobre o crescente casos de dengue no país, mas também especialmente no nosso município, pensou-se no desenvolvimento dessa aplicação como possível ferramente tecnológica para auxiliar na solução do problema pesquisado. 
                 </Typography>
-                <Typography component='p' textAlign='justify' m='0.5em 0' className={rubik.className}>
+                <Typography component='p' textAlign='justify' m='0.5em 0' className={rubik.className} color='331800'>
                     A ideia da aplicação é poder fornecer à administração pública relatos de possíveis focos de dengue. Qualquer pessoa com acesso a internet no celular pode usar a plataforma e com poucos dados do local, informar que essa localização é um risco potencial para nossa cidade. Com mais dados, os administradores municipais podem então agir com mais eficiência na eliminação de focos de dengue. 
                 </Typography>              
             </Box>
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,14 +12,14 @@ export default function Page() {
       <>
         <Header />
         <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%', gap: '2em', mb: '2em'}}>
-          <Typography variant="h4" textAlign='center' marginTop='24px' fontWeight='700' className={rubik.className}>
+          <Typography variant="h4" textAlign='center' marginTop='24px' fontWeight='700' className={rubik.className} color='1b0000'>
             Canoas contra a dengue
           </Typography>
-          <Typography color='gray' textAlign='center' width='90%' className={rubik.className}>
+          <Typography color='331800' textAlign='center' width='90%' className={rubik.className} >
             Aqui você consegue relatar lugares com focos de dengue na nossa cidade. 
           </Typography>
           <Image alt="mapa de cidade" src={map} style={{maxWidth: '80%', height: 'auto'}}/>
-          <Typography color='gray' textAlign='center' width='90%' className={rubik.className}>
+          <Typography color='331800' textAlign='center' width='90%' className={rubik.className}>
             Faça seu login usando o .gov e faça seu relato de lugares da cidade com possíveis focos de dengue.
           </Typography>
         </Box>
